f11a56205d7a42e100778a01551d39c70d18ad3d
Maxim Harder <info@maxim-harder.de>
2022-09-14 07:34:09 +0200

    Update _settings.php

--- upload/engine/ajax/maharder/maharder/_settings.php
+++ upload/engine/ajax/maharder/maharder/_settings.php
@@ -1,34 +1,37 @@
 <?php
 
-if (!$is_logged) {
-	exit('error');
-}
+	if (!$is_logged) {
+		exit('error');
+	}
 
-if ('' == $_REQUEST['user_hash'] or $_REQUEST['user_hash'] != $dle_login_hash) {
-	exit('error');
-}
+	if ('' == $_REQUEST['user_hash'] or $_REQUEST['user_hash'] != $dle_login_hash) {
+		exit('error');
+	}
 
-if (!mkdir($concurrentDirectory = ENGINE_DIR . '/inc/maharder/_config', 0777, true)
-	&& !is_dir(
-		$concurrentDirectory
-	)) {
-	LogGenerator::generate_log('maharder', 'save_setting', sprintf('Папка "%s" не была создана',
-		$concurrentDirectory));
-}
-$file = $concurrentDirectory.'/'.$_POST['module'].'.json';
-
-
-if (empty($data['list_count']) || !isset($data['list_count'])) {
-	$data['list_count'] = $config['news_number'];
-}
-
-if (empty($data['cache_timer']) || !isset($data['cache_timer'])) {
-	$data['cache_timer'] = 60;
-}
+	if (!mkdir($concurrentDirectory = ENGINE_DIR . '/inc/maharder/_config', 0777, true)
+	    && !is_dir(
+			$concurrentDirectory
+		)) {
+		LogGenerator::setLogs(1);
+		LogGenerator::generate_log('maharder', 'save_setting', sprintf('Папка "%s" не была создана',
+		                                                               $concurrentDirectory));
+	}
+	$file = $concurrentDirectory.'/'.$_POST['module'].'.json';
 
 
-$data = json_encode($data, JSON_UNESCAPED_UNICODE);
-file_put_contents($file, $data);
-clear_cache();
+	if (empty($save_con['list_count']) || !isset($save_con['list_count'])) {
+		$save_con['list_count'] = $config['news_number'];
+	}
 
-echo 'ok';
+	if (empty($save_con['cache_timer']) || !isset($save_con['cache_timer'])) {
+		$save_con['cache_timer'] = 60;
+	}
+
+	if (empty($save_con['theme']) || !isset($save_con['theme'])) {
+		$save_con['theme'] = 'light';
+	}
+
+	file_put_contents($file, json_encode($save_con, JSON_UNESCAPED_UNICODE));
+	clear_cache();
+
+	echo 'ok';

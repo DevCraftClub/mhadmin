5d2910511a45196d2ff3f68fd2464c64be2d6507
Maxim Harder <maximharder@bundeswehr.org>
2025-02-10 11:03:15 +0100

    Добавление новых фильтров (rangeFilter и dateRangeFilter) для TwigFilter

--- upload/engine/inc/maharder/_includes/classes/TwigFilter.php
+++ upload/engine/inc/maharder/_includes/classes/TwigFilter.php
@@ -32,23 +32,17 @@ class TwigFilter {
 	 * @see getRepository() Используется внутри метода createFilterChoices для получения данных.
 	 * @see __() Используется в createFilterChoices для перевода текста.
 	 */
-	public function createFilter(
-		string $column_name,
-		#[ExpectedValues(values: ['select', 'text', 'tags', 'checkbox'])]
-		string $type,
-		string $label,
-		?string $select_value = null,
-		?array $choices = null
-	): array {
+	public function createFilter(string $column_name, #[ExpectedValues(values: ['select', 'text', 'tags', 'checkbox'])]
+	string                              $type, string $label, ?string $select_value = null, ?array $choices = null): array {
 		$filter = [
 			$column_name => [
-				'type' => $type,
+				'type'  => $type,
 				'label' => $label
 			]
 		];
 
 		if (in_array($type, ['select', 'tags'])) {
-			$select_value = $select_value ?? $column_name;
+			$select_value                    = $select_value ?? $column_name;
 			$filter[$column_name]['choices'] = $choices ?: $this->createFilterChoices($column_name, $select_value);
 		}
 
@@ -63,9 +57,10 @@ class TwigFilter {
 	 *
 	 * @return array Ассоциативный массив, где ключи — значения фильтров, а значения — отображаемые имена фильтров.
 	 *
-	 * @see __ Используется для локализации первой строки массива фильтров.
+	 * @throws Throwable
 	 * @see translate Вызванный косвенно через функцию __.
 	 * @see getRepository Используется для получения данных из базы через методы репозитория.
+	 * @see __ Используется для локализации первой строки массива фильтров.
 	 */
 	public function createFilterChoices($name, $select): array {
 		$filter = [
@@ -98,7 +93,7 @@ class TwigFilter {
 	public static function getSort(string $sort): string {
 		return match ($sort) {
 			'asc', 'ASC' => SelectQuery::SORT_ASC,
-			default => SelectQuery::SORT_DESC
+			default      => SelectQuery::SORT_DESC
 		};
 	}
 
@@ -112,22 +107,24 @@ class TwigFilter {
 	 * - 'sort' — очистка данных аналогично предыдущим, но с приведением к верхнему регистру.
 	 *
 	 * Метод предоставляет возможность добавлять пользовательские фильтры с помощью аргумента `$additionalFilters`.
-	 * Если значение пользовательского фильтра равно `null`, используется стандартный фильтр `FILTER_SANITIZE_FULL_SPECIAL_CHARS`.
+	 * Если значение пользовательского фильтра равно `null`, используется стандартный фильтр
+	 * `FILTER_SANITIZE_FULL_SPECIAL_CHARS`.
 	 *
-	 * @param array $additionalFilters Ассоциативный массив дополнительных фильтров, где ключи — названия,
+	 * @param array $additionalFilters  Ассоциативный массив дополнительных фильтров, где ключи — названия,
 	 *                                  а значения — их типы.
-	 *                                  Если значение `null`, применяется стандартный `FILTER_SANITIZE_FULL_SPECIAL_CHARS`.
+	 *                                  Если значение `null`, применяется стандартный
+	 *                                  `FILTER_SANITIZE_FULL_SPECIAL_CHARS`.
 	 *
 	 * @return array Ассоциативный массив, содержащий объединенные стандартные и дополнительные фильтры.
 	 */
 	public static function getDefaultFilters(array $additionalFilters = []): array {
 		$defaultFilters = [
-			'page'          => FILTER_VALIDATE_INT,
-			'mod'           => FILTER_SANITIZE_FULL_SPECIAL_CHARS | FILTER_NULL_ON_FAILURE,
-			'action'        => FILTER_SANITIZE_FULL_SPECIAL_CHARS | FILTER_NULL_ON_FAILURE,
-			'sites'         => FILTER_SANITIZE_FULL_SPECIAL_CHARS | FILTER_NULL_ON_FAILURE,
-			'order'         => FILTER_SANITIZE_FULL_SPECIAL_CHARS | FILTER_NULL_ON_FAILURE,
-			'sort'          => FILTER_SANITIZE_FULL_SPECIAL_CHARS | CASE_UPPER | FILTER_NULL_ON_FAILURE
+			'page'   => FILTER_VALIDATE_INT,
+			'mod'    => FILTER_SANITIZE_FULL_SPECIAL_CHARS | FILTER_NULL_ON_FAILURE,
+			'action' => FILTER_SANITIZE_FULL_SPECIAL_CHARS | FILTER_NULL_ON_FAILURE,
+			'sites'  => FILTER_SANITIZE_FULL_SPECIAL_CHARS | FILTER_NULL_ON_FAILURE,
+			'order'  => FILTER_SANITIZE_FULL_SPECIAL_CHARS | FILTER_NULL_ON_FAILURE,
+			'sort'   => FILTER_SANITIZE_FULL_SPECIAL_CHARS | CASE_UPPER | FILTER_NULL_ON_FAILURE
 		];
 
 		foreach ($additionalFilters as $key => $value) {
@@ -163,6 +160,68 @@ class TwigFilter {
 		$this->repository = $repository;
 	}
 
+	/**
+	 * Создает фильтр диапазона на основе указанных данных столбца.
+	 *
+	 * @param string               $column_name Название столбца, данные которого используются для определения
+	 *                                          диапазона.
+	 * @param string               $label       Метка для фильтра диапазона.
+	 *
+	 * @return array Возвращает массив конфигурации фильтра диапазона, включающий название столбца, метку, начальную и
+	 *               конечную точки диапазона.
+	 *
+	 * @global RepositoryInterface $repository  Репозиторий, использующийся для выполнения запросов.
+	 *
+	 * @see getRepository() Метод получения текущего репозитория.
+	 */
+	public function createRangeFilter(string $column_name, string $label): array {
+		$data  = (array)$this->getRepository()->select()->columns([$column_name])->orderBy($column_name)->sortBy(
+			SelectQuery::SORT_ASC
+		)->fetchAll();
+		$first = $data[0][$column_name];
+		$last  = $data[count($data) - 1][$column_name];
 
+		return [
+			$column_name => [
+				'type'  => 'range',
+				'label' => $label,
+				'from'  => $first,
+				'to'    => $last
+			]
+		];
+	}
+
+	/**
+	 * Создает фильтр диапазона дат для указанного столбца.
+	 *
+	 * Фильтр включает информацию о диапазоне дат, включающую минимальное и максимальное значение
+	 * на основе данных, извлеченных из репозитория.
+	 *
+	 * @param string               $column_name Название столбца, по которому создается фильтр.
+	 * @param string               $label       Метка, которая будет отображаться для фильтра.
+	 *
+	 * @return array Ассоциативный массив с описанием фильтра, содержащий тип, метку, минимальное и максимальное
+	 *               значения.
+	 *
+	 * @see \TwigFilter::getRepository Используется для получения репозитория данных.
+	 * @see \TwigFilter::setRepository Используется для установки репозитория данных.
+	 * @global RepositoryInterface $repository  Репозиторий, с которым связан класс.
+	 */
+	public function createDateRangeFilter(string $column_name, string $label): array {
+		$data  = (array)$this->getRepository()->select()->columns([$column_name])->orderBy($column_name)->sortBy(
+			SelectQuery::SORT_ASC
+		)->fetchAll();
+		$first = $data[0][$column_name];
+		$last  = $data[count($data) - 1][$column_name];
+
+		return [
+			$column_name => [
+				'type'  => 'daterange',
+				'label' => $label,
+				'min'   => $first,
+				'max'   => $last
+			]
+		];
+	}
 
 }

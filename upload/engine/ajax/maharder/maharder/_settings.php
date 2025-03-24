bb81441897db4d6cad62a91d9871472f7e552df1
Maxim Harder <info@maxim-harder.de>
2025-03-24 12:45:55 +0100

    ✨ fix: Исправляет форматирование и добавляет новые настройки
    Упрощает форматирование массива настроек для лучшей читаемости.  
    Добавляет новую настройку `logs_telegram_type` для расширения  
    возможностей конфигурации. Исправляет сохранение настроек в  
    методе `saveConfig`. Обновляет журнал изменений с описанием  
    новых исправлений и улучшений.

--- upload/engine/ajax/maharder/maharder/_settings.php
+++ upload/engine/ajax/maharder/maharder/_settings.php
@@ -3,22 +3,26 @@
 global $parsedData, $config;
 
 $concurrentDirectory = ENGINE_DIR . '/inc/maharder/_config';
-DataManager::createDir(service : 'maharder', module : 'save_setting', _path : $concurrentDirectory);
+DataManager::createDir(service: 'maharder', module: 'save_setting', _path: $concurrentDirectory);
 
-$settings = filter_var_array($parsedData, [
-	'list_count' => FILTER_VALIDATE_INT,
-	'cache_path' => FILTER_SANITIZE_FULL_SPECIAL_CHARS,
-	'cache_timer' => FILTER_VALIDATE_INT,
-	'language' => FILTER_SANITIZE_FULL_SPECIAL_CHARS,
-	'locales_path' => FILTER_SANITIZE_FULL_SPECIAL_CHARS,
-	'logs' => FILTER_VALIDATE_BOOLEAN,
-	'logs_db' => FILTER_VALIDATE_BOOLEAN,
-	'logs_telegram' => FILTER_VALIDATE_BOOLEAN,
-	'logs_telegram_api' => FILTER_SANITIZE_FULL_SPECIAL_CHARS,
-	'logs_telegram_channel' => FILTER_SANITIZE_FULL_SPECIAL_CHARS,
-	'cache_icon' => FILTER_VALIDATE_BOOLEAN,
-	'theme' => FILTER_SANITIZE_FULL_SPECIAL_CHARS,
-]);
+$settings = filter_var_array(
+	$parsedData,
+	[
+		'list_count'            => FILTER_VALIDATE_INT,
+		'cache_path'            => FILTER_SANITIZE_FULL_SPECIAL_CHARS,
+		'cache_timer'           => FILTER_VALIDATE_INT,
+		'language'              => FILTER_SANITIZE_FULL_SPECIAL_CHARS,
+		'locales_path'          => FILTER_SANITIZE_FULL_SPECIAL_CHARS,
+		'logs'                  => FILTER_VALIDATE_BOOLEAN,
+		'logs_db'               => FILTER_VALIDATE_BOOLEAN,
+		'logs_telegram'         => FILTER_VALIDATE_BOOLEAN,
+		'logs_telegram_api'     => FILTER_SANITIZE_FULL_SPECIAL_CHARS,
+		'logs_telegram_channel' => FILTER_SANITIZE_FULL_SPECIAL_CHARS,
+		'logs_telegram_type'    => FILTER_SANITIZE_FULL_SPECIAL_CHARS,
+		'cache_icon'            => FILTER_VALIDATE_BOOLEAN,
+		'theme'                 => FILTER_SANITIZE_FULL_SPECIAL_CHARS,
+	]
+);
 
 if (empty($settings['list_count']) || !isset($settings['list_count'])) {
 	$settings['list_count'] = $config['news_number'];
@@ -44,7 +48,7 @@ if (empty($settings['theme']) || !isset($settings['theme'])) {
 	$settings['theme'] = 'light';
 }
 
-DataManager::saveConfig('maharder',  $settings);
+DataManager::saveConfig('maharder', $settings);
 clear_cache();
 
 echo 'ok';
